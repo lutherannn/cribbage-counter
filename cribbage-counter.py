b587318a103ac3ff28f353dd166d53abee41b9d7
lutherannn <michael.eskelin@gmaiil.com>
2022-10-19 05:45:14 -0400

    started to count pairs

--- cribbage-counter.py
+++ cribbage-counter.py
@@ -7,7 +7,7 @@ suits = []
 print("Format your hand with the card number and the suit with spaces between.")
 print("For example: 2c 3d 4s 5h")
 print(
-    "Would be: The 2 of clubs, 3 of diamonds, 4 of spades, 5 of hearts, and the 6 of hearts"
+    "Would be: The 2 of clubs, 3 of diamonds, 4 of spades, 5 of hearts."
 )
 handInput = input("Enter cards in hand: ")
 cribCard = input("Enter the crib card in the same format as you entered your hand: ")
@@ -35,4 +35,10 @@ for x in handInput.split():
 if len(cribCard) != 2:
     print("Invalid crib card, the crib card should only be one card.")
 
+#Find pairs, each is worth 2 points
+for x in numbers:
+    if numbers.index(x) != 3:
+        if x == numbers[numbers.index(x) + 1]:
+            print("Pair found.")
+            print(x, numbers[numbers.index(x) + 1])
 print(hand, numbers, suits)
